41c21cc17c27d89714a144f988dd1e001ea37d3a
Ben Balter <ben@balter.com>
2023-03-14 16:54:03 -0400

    initial gmail stats

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { promises as fs } from "fs";
 import path from "path";
 import process from "process";
 import { authenticate } from "@google-cloud/local-auth";
-import { google, calendar_v3 } from "googleapis";
+import { google, calendar_v3, gmail_v1 } from "googleapis";
 import { stringify } from "csv-stringify";
+import { urlToHttpOptions } from "url";
+import addrs from "email-addresses";
 
-const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar.readonly",
+  "https://www.googleapis.com/auth/gmail.readonly",
+];
 const TOKEN_PATH = path.join(process.cwd(), "token.json");
 const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
@@ -19,6 +24,29 @@ const YEARS = [
 
 // Internal domain (defaults to your email domain)
 const DOMAIN = EMAIL.split("@")[1];
+
+// "From" addresses to exclude from the email search
+const EXCLUDE_FROM = [
+  `notifications@${DOMAIN}`,
+  `@users.noreply.${DOMAIN}`,
+  `notifications@support.${DOMAIN}`,
+  `shop@${DOMAIN}`,
+  `noreply@${DOMAIN}`,
+  `enterprise@${DOMAIN}`,
+  `support@${DOMAIN}`,
+];
+
+const EXCLUDE_TO = [
+  `@noreply${DOMAIN}`,
+  `halp-noreply@${DOMAIN}`,
+  `@lists.${DOMAIN}`,
+  `all@${DOMAIN}`,
+  `government@${DOMAIN}`,
+  `@reply.${DOMAIN}`,
+  `haps@${DOMAIN}`,
+];
+
+const EXCLUDE_SUBJECT = ["Hangout with ", "Thank you for your message"];
 
 // Represents a single event on your calendar
 class event {
@@ -111,6 +139,135 @@ class event {
   }
 }
 
+class thread {
+  data: gmail_v1.Schema$Thread;
+  client: gmail_v1.Gmail;
+  messages: message[] | undefined;
+
+  constructor(data: gmail_v1.Schema$Thread, client: gmail_v1.Gmail) {
+    this.data = data;
+    this.client = client;
+  }
+
+  // Retrieves the threads messages
+  async getMessages(): Promise<message[] | undefined> {
+    if (this.messages) return this.messages;
+
+    console.log(`Getting messages for thread ${this.data.id}`);
+    const res = await this.client.users.threads.get({
+      userId: "me",
+      id: this.data.id || "",
+      format: "metadata",
+    });
+    this.messages = res.data.messages?.map((m) => new message(m));
+    return this.messages;
+  }
+
+  toRows() {
+    return this.messages?.map((m) => m.toRow());
+  }
+}
+
+class message {
+  data: gmail_v1.Schema$Message;
+
+  constructor(data: gmail_v1.Schema$Message) {
+    this.data = data;
+  }
+
+  // Returns the message headers
+  headers(): gmail_v1.Schema$MessagePartHeader[] | undefined {
+    return this.data.payload?.headers;
+  }
+
+  getHeader(name: string) {
+    const header = this.headers()?.find((h) => {
+      return h.name === name;
+    });
+    return header?.value;
+  }
+
+  subject() {
+    return this.getHeader("Subject");
+  }
+
+  to() {
+    const to = this.getHeader("To");
+    const addr = addrs.parseAddressList(to || "");
+    if (!addr) {
+      return;
+    }
+
+    const addresses = addr.map((a) => {
+      if (a.type === "group") {
+        return a.addresses;
+      } else {
+        return [a];
+      }
+    });
+
+    return addresses.flat();
+  }
+
+  from() {
+    const from = this.getHeader("From");
+    const addr = addrs.parseOneAddress(from || "");
+
+    if (addr?.type === "group") {
+      return addr.addresses[0];
+    } else {
+      return addr;
+    }
+  }
+
+  isSender() {
+    const from = this.from();
+
+    if (!from) {
+      return false;
+    }
+
+    return from.address.replace(".", "") === EMAIL;
+  }
+
+  isInternal() {
+    return (
+      this.from()?.domain === DOMAIN &&
+      this.to()?.every((to) => {
+        return to.domain === DOMAIN;
+      })
+    );
+  }
+
+  excludedFrom() {
+    return EXCLUDE_FROM.includes(this.from()?.address || "");
+  }
+
+  excludedTo() {
+    return this.to()?.some((to) => {
+      return EXCLUDE_TO.includes(to.address);
+    });
+  }
+
+  shouldInclude() {
+    return this.isInternal() && !this.excludedFrom() && !this.excludedTo();
+  }
+
+  toRow() {
+    return {
+      date: this.data.internalDate,
+      year: new Date(Number(this.data.internalDate)).getFullYear(),
+      month: new Date(Number(this.data.internalDate)).getMonth() + 1,
+      subject: this.subject(),
+      from: this.from()?.address,
+      to: this.to()
+        ?.map((to) => to.address)
+        .join(","),
+      isSender: this.isSender(),
+    };
+  }
+}
+
 /**
  * Reads previously authorized credentials from the save file.
  *
@@ -187,9 +344,63 @@ async function getEvents(auth, year: number) {
   return items.map((item) => new event(item));
 }
 
+async function getThreads(auth, year: number) {
+  const gmail = google.gmail({ version: "v1", auth });
+  const fromFilter = EXCLUDE_FROM.map((email) => `-"from:${email}"`).join(" ");
+  const toFilter = EXCLUDE_TO.map((email) => `-"to:${email}"`).join(" ");
+  const subjectFilter = EXCLUDE_SUBJECT.map(
+    (subject) => `-"subject:${subject}"`
+  ).join(" ");
+  const q = `to:(${DOMAIN}) from:(${DOMAIN}) ${fromFilter} ${toFilter} ${subjectFilter} -{"invite.ics"} after:${year}/01/01 before:${year}/12/31`;
+
+  console.log(`Listing threads using the following query: ${q}`);
+  const res = await gmail.users.threads.list({
+    userId: "me",
+    q,
+    maxResults: 500,
+    includeSpamTrash: false,
+  });
+
+  const threads = res.data.threads;
+  if (!threads || threads.length === 0) {
+    console.log("No threads found.");
+    return;
+  }
+
+  console.log(`Found ${threads.length} threads`);
+  return threads.map((t) => new thread(t, gmail));
+}
+
 // The main event
 async function run() {
   const client = await authorize();
+
+  const threads = await getThreads(client, 2013);
+
+  if (threads) {
+    let threadsFiltered: thread[] = [];
+    for (const t of threads) {
+      const messages = await t.getMessages();
+      if (messages && messages.every((m) => m.shouldInclude())) {
+        threadsFiltered = [...threadsFiltered, t];
+      }
+    }
+
+    console.log(`Filtered to ${threadsFiltered.length} threads`);
+    let rows = threadsFiltered.map((t) => t.toRows()).flat();
+    stringify(
+      rows,
+      {
+        header: true,
+      },
+      function (err, output) {
+        console.log(output);
+        fs.writeFile(`${__dirname}/../messages.csv`, output);
+      }
+    );
+  }
+
+  /* 
   let events: event[] = [];
 
   // Get events for each year
@@ -214,7 +425,7 @@ async function run() {
       console.log(output);
       fs.writeFile(`${__dirname}/../data.csv`, output);
     }
-  );
+  );*/
 }
 
 run();
